bed6cdcd1c4a1668f7a224c60fbc715976066809
Vedant Tapkir <47036160+Octaflick@users.noreply.github.com>
2026-04-23 10:30:44 +0530

    Add: Functions directory | Refactor: Dynamic Data for Sidebar

---
+++ src/functions/preFetch.js
@@ -0,0 +1,42 @@
+export function collectivesData() {
+	let data = [
+		{
+			img: `img`,
+			org: `AWS`,
+			members: `39k Members`,
+			desc: `A collective for developers who utilize Amazon Web Services'
+						infrastructure...`,
+		},
+		{
+			img: `img`,
+			org: `Microsoft Azure`,
+			members: `30k Members`,
+			desc: `A collective for developers to engage, share, and learn about Microsoft Azure’s...`,
+		},
+		{
+			img: `img`,
+			org: `Mobile Develpoment`,
+			members: `29k Members`,
+			desc: `A collective for developers who want to share their knowledge and learn more...`,
+		},
+	];
+
+	return data;
+}
+
+export function campusTalksData() {
+	let data = [
+		{ title: "How to get multiple agents to play nice at scale", url: `` },
+		{ title: "We still need developer communities", url: `` },
+	];
+
+	return data;
+}
+
+export function featuredData() {
+	let data = [
+		{ title: "Retiring the beta site", url: `` },
+		{ title: "Policy: Generative AI (e.g., ChatGPT) is banned", url: `` },
+	]
+    return data
+}
